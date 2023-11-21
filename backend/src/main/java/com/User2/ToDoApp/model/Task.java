4e3d80321886284b7e90933fccdbcdca7c0241e9
Disanayaka AAC <charair742@gmail.com>
2023-11-21 11:37:44 +0530

    testing

--- backend/src/main/java/com/User2/ToDoApp/model/Task.java
+++ backend/src/main/java/com/User2/ToDoApp/model/Task.java
@@ -18,7 +18,7 @@ import java.util.Date;
 public class Task{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long taskId;
     private String title;
     private String description;
